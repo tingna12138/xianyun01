3ebd378a860c1350834545ac49985ceaa7a9c7d6
LovlyGir <lovlygir@163.com>
2019-11-13 15:30:26 +0800

    使用nprogress插件练习1

---
+++ pages/nprogress/nprogress.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      nprrogress测试
+  </div>
+</template>
+
+<script>
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+console.log('123')
+ NProgress.start()
+
+export default {
+   mounted(){
+       setTimeout(()=>{
+           NProgress.done();
+       },4000)
+   }
+}
+</script>
+
+<style>
+
+</style>
